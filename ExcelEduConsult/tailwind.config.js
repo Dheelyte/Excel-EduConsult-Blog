982c1be334568d0f89a52f71e8a2bcf63a8d9db5
@MILEZ <levvyon@gmail.com>
2024-03-27 02:07:16 +0100

    designing homepage

--- ExcelEduConsult/tailwind.config.js
+++ ExcelEduConsult/tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   content: ["./templates/**/*.{html,js}"],
   theme: {
     extend: {
-      
+      color: {
+        coverColor: 'rgb(165, 233, 255)',
+      },
+      fontfamily: {
+        'newFont1': ['Raleway'],
+        'newFont2': ['Nunito'],
+      },
     },
   },
   plugins: [],
